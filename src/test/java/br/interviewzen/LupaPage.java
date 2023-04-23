660d5525d63fc831ee25c0a285ca3d83a7e001b8
Wagner Fontes <104961653+WagnerFontes@users.noreply.github.com>
2023-04-23 12:28:37 -0300

    Update LupaPage.java

--- src/test/java/br/interviewzen/LupaPage.java
+++ src/test/java/br/interviewzen/LupaPage.java
@@ -1,9 +1,7 @@
 package br.interviewzen;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import br.steps.CriarContaSteps;
 
 public class LupaPage extends LupaSteps {
 	private static WebDriver driver;
