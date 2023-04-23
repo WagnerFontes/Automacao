02a4ea3fdad17398fec7a63ab2573dc728cbf41e
Wagner Fontes <104961653+WagnerFontes@users.noreply.github.com>
2023-04-23 12:31:17 -0300

    Update LupaSteps.java

--- src/test/java/br/interviewzen/LupaSteps.java
+++ src/test/java/br/interviewzen/LupaSteps.java
@@ -2,6 +2,7 @@ package br.interviewzen;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.is.En;
@@ -12,7 +13,7 @@ public class LupaSteps {
 	private static WebDriver driver;
 	private LupaPage page;
 
-	// **** Cenario: Cadastro com sucesso ****
+	// **** Clicar no icone lupa ****
 
 	@Given("que o usuario clique no icone de lupa")
 	public void queOUsuarioCliqueNoIconeDeLupa() {
@@ -24,8 +25,8 @@ public class LupaSteps {
 		Assert.assertTrue(driver.findElement(By.id("search-field")).isSelected());	
 
 	}
-	@End("o bot„o pesquisar")
-	public void oBot„oesquisar() {
+	@End("o bot√£o pesquisar")
+	public void oBot√£oesquisar() {
 		page.setCliqueBotaoPesquisar();	
 
 	}
@@ -37,7 +38,7 @@ public class LupaSteps {
 		page.setCliqueBotaoPesquisar();	
 
 	}
-	@Then("o usuario È redirecionado para a tela com o logo agi")
+	@Then("o usuario √© redirecionado para a tela com o logo agi")
 	public void oUsuarioERedirecionadoParaAOutraTela() {
 		Assert.assertTrue(driver.findElement(By.class("archive-title")).isSelected());
 	}
